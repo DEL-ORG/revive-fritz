fdd7d79135378d2e46260523b40a285e5cd10203
fritzmney <fritz@google.com>
2024-03-26 14:39:31 +0100

    test

--- EspressoShop-application/EspressoShop.Web/Models/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EspressoShop.Web.Models
-{
-    public class ApplicationUser
-    {
-        public string Email { get; set; }
-        public string FullName { get; set; }
-    }
-}
--- EspressoShop-application/EspressoShop.Web/Services/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EspressoShop.Web.Services
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
